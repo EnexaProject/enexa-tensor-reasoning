aa606200d2652741a582114dfc0e5a532ca05eb4
Alex Goeßmann <alex.goessmann@web.de>
2023-11-02 15:36:23 +0100

    Specified requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-    name="tensor-reasoning",
+    name="tensorreasoning",
     version="0.0",
     author="Alex Goeßmann",
+    description="A package for reasoning with tensors",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
-        "numpy",
-        "pandas",
-        "pgmpy"
-    ]
+        "numpy>=1.23.4",
+        "pandas>=1.5.3",
+        "pgmpy>=0.1.24"
+    ],
+    python_requires=">=3.8.5",
+    py_modules=["tensorreasoning"],
+    package_dir={'':'tensor_reasoning'},
 )
