a9647bc71c6c2d58f4b1d718ef830061e86699df
Alex Goeßmann <alex.goessmann@web.de>
2023-11-06 10:21:52 +0100

    Generation of Accounting KG added

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ Represent Knowledge Graphs as tensors to perform logic calculus and regression.
 
 ## Usage
 
-Examples can be found in `demonstration`.
+This module can be used for different tasks, as demonstrated in the `examples` folder. 
 
-## Contents
+### Generation of Random Knowledge Graphs
+
+Given a set of logical formulas a Markov Logic Network can be created using the Basis Calculus. One can then sample from the model to generate random data, in this case interpreted as a Random Knowledge Graph.
+
+An example can be found in `examples/generation/generate_accounting_kg.py`
+
+### Learning of logical formulas
+
+Given a Knowledge Graph and positive and negative examples (each a pair of individuals), one can learn a logical formula true on the positive and false on the negative examples. To this end optimization via Alternating Least Squares has been implemented.
+Examples can be found in `examples/learning/`.
+
+
+## Submodules
 
 The repository contains these submodules, each performing a dedicated task.
 
@@ -26,14 +38,14 @@ Basis Calculus: `BasisCalculus` main class for basis-vector-based calculus of lo
 
 Expression Calculus: Evaluation of expressions given dictionaries of `CoordinateCalculus`/`BasisCalculus` objects.
 
-### ALS
+### Optimization
 
 `generalized_als.py`: Performing the Alternating Least Squares.
 
-### Expression Learning
+### Learning
 
 `expression_learning.py` Class `ExpressionLearner` is a wrapper of the representation and optimization module to learn logical formulas.
 
-### Graphical Models
+### Models
 
 `create_mln.py` Creates a Markov Logic Network base
--- examples/generation/generate_accounting_kg.py
+++ examples/generation/generate_accounting_kg.py
@@ -1,21 +1,22 @@
 from tnreason.model import generate_test_data as gtd
 from tnreason.representation import csv_to_ttl as ctt
+from tnreason.logic import expression_generation as eg
 
-example_expression_dict = {
-    "e0": ["Rechnung(x)", 1.5],
-    "e1": [["not", "Ausgangsrechnung(x)"], 1.5],
-    "e2": [["Ausgangsrechnung(x)", "and", "zuMandant(x,y)"], 2],
-    "e3": [[["not", "Ausgangsrechnung(y)"], "and", ["not", "Rechnung(x)"]], 2],
-    "e4": [["Ausgangsrechnung(x)", "and", "Rechnung(y)"], 2],
-    "e5": ["Spezialrechnung(x)", 1.5],
-    "e6": [["versandt(y,x)", "and", "Ausgangsrechnung(x)"], 2],
-    "e7": [["bearbeitet(y,x)", "and", "enthaelt(x,z)"], 1.2]
+## Each rule is stored as value in the dictionary, and has format [list of premises, head, MLN weight]
+example_rule_dict = {
+    "r0": [["Unterschrank(z)"], "Moebel(z)", 1.5],
+    "r1": [["hatLeistungserbringer(x,y)", "versandterBeleg(y,x)"], "Ausgangsrechnung(x)", 1.5],
+    "r2": [["Ausgangsrechnung(x)", "versandterBeleg(y,x)", "Bautischlerei(y)", "hatBelegzeile(x,z)", "Moebel(z)", "verbuchtDurch(z,q)"],"Umsatzerloese(q)", 1.5]
 }
 
+## We transform the rules into propositional formulas containing negations and conjunctions only
+example_expression_dict = {key:[eg.generate_list_from_rule(value[0],value[1]), value[2]] for (key,value) in example_rule_dict.items()}
+
+## A list of facts is generated using Gibbs Sampling of the associated Markov Logic Network
 dataNum = 10
 factDf, pairDf = gtd.generate_factDf_and_pairDf(example_expression_dict, sampleNum=dataNum, prefix="tev:")
 
-savePath = "./examples/model/synthetic_test_data/"
+savePath = "./examples/generation/synthetic_test_data/"
 factDf.to_csv(savePath+"generated_factDf.csv")
 
 g = ctt.dataframe_to_kg(factDf)
