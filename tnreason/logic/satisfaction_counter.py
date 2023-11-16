673bacdf1e6f8035a2f27534f042a731ced2f37e
Alex Goeßmann <alex.goessmann@web.de>
2023-11-16 18:21:43 +0100

    Bugfix in Satisfaction Counter by adding touchedvariables

--- tnreason/logic/satisfaction_counter.py
+++ tnreason/logic/satisfaction_counter.py
@@ -1,33 +1,42 @@
 from tnreason.logic import expression_calculus as ec
 
 def count_satisfaction(expression):
-    conjunctionList = compute_conjustionList(expression)
+    conjunctionList = compute_conjunctionList(expression)
     count = 0
     for entry in conjunctionList:
-        count += 2**len(entry[0])*entry[1]
+        count += 2**len(entry[0])*entry[2]
     return count
 
-def compute_conjustionList(expression):
+def compute_conjunctionList(expression):
     if type(expression) == str:
-        return [[[],1]]
+        return [[[],[expression],1]]
     if expression[0] == "not":
         variables = combine_unique(ec.get_variables(expression),[])
-        conjunctionList1 = compute_conjustionList(expression[1])
-        conjunctionList = [[variables,1]]
+        conjunctionList1 = compute_conjunctionList(expression[1])
+        conjunctionList = [[variables,[],1]]
         for entry in conjunctionList1:
-            conjunctionList.append([entry[0],-1*entry[1]])
+            conjunctionList.append([entry[0],entry[1],-1*entry[2]])
         return conjunctionList
     elif expression[1] == "and":
-        conList0 =  compute_conjustionList(expression[0])
-        conList2 =  compute_conjustionList(expression[2])
+        conList0 =  compute_conjunctionList(expression[0])
+        conList2 =  compute_conjunctionList(expression[2])
 
         conjunctionList = []
         for entry0 in conList0:
             for entry2 in conList2:
-                conjunctionList.append([combine_unique(entry0[0],entry2[0]), entry0[1]*entry2[1]])
+                conjunctionList.append(merge_entries(entry0,entry2))
         return conjunctionList
     else:
         raise ValueError("Expression {} not understood.".format(expression))
+
+def merge_entries(entry0, entry2):
+    touchedVariables = entry0[1]+entry2[1]
+    preuntouchedVariables = combine_unique(entry0[0],entry2[0])
+    untouchedVariables = []
+    for variable in preuntouchedVariables:
+        if variable not in touchedVariables:
+            untouchedVariables.append(variable)
+    return [untouchedVariables, touchedVariables, entry0[2]*entry2[2]]
 
 def combine_unique(list1,list2):
     list = []
@@ -42,3 +51,6 @@ if __name__ == "__main__":
     assert count_satisfaction(["not",["test","and","sledz"]])==3, "Satisfaction Counter wrong"
     assert count_satisfaction(["not",["losos","and",["sledz","and","sikorka"]]])==7, "Satisfaction Counter wrong"
     assert count_satisfaction(["not",["losos","and",["losos","and","sikorka"]]])==3, "Satisfaction Counter wrong"
+
+    print(compute_conjunctionList(["jaszczur","and",["not",["jaszczur","and","sledz"]]]))
+    assert count_satisfaction(["jaszczur","and",["not",["jaszczur","and","sledz"]]])==1, "Satisfaction Counter wrong"
