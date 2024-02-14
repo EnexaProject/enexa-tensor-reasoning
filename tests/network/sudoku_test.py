4544eb4391ac8f0bd7e60d2d9cece7d75b8d49b2
Alex Goeßmann <alex.goessmann@web.de>
2024-02-14 19:37:11 +0100

    Added positional constraints and vizualization to Sudoko Solver

--- tests/network/sudoku_test.py
+++ tests/network/sudoku_test.py
@@ -2,11 +2,14 @@ from tnreason.network import constraint_propagation as cp
 from tnreason.model import formula_tensors as ft
 from tnreason.model import create_cores as crc
 
+import numpy as np
+
 
 def get_sudoku_constraints(num=3):
     return {**get_column_constraints(num),
             **get_row_constraints(num),
-            **get_squares_constraints(num)}
+            **get_squares_constraints(num),
+            **get_position_constraints(num)}
 
 
 def get_column_constraints(num=3):
@@ -52,11 +55,43 @@ def get_squares_constraints(num=3):
     return categoricalConstraints
 
 
-coreDict = {}
+def get_position_constraints(num=3):
+    categoricalConstraints = {}
+    for r1 in range(num):
+        for r2 in range(num):
+            for c1 in range(num):
+                for c2 in range(num):
+                    catVarKey = "pos_" + str(r1) + "_" + str(r2) + "_" + str(c1) + "_" + str(c2)
+                    categoricalConstraints[catVarKey] = []
+                    for n in range(num ** 2):
+                        categoricalConstraints[catVarKey].append(
+                            "a_" + str(r1) + "_" + str(r2) + "_" + str(c1) + "_" + str(c2) + "_" + str(n))
+    return categoricalConstraints
 
-num = 4
 
+## Visualization
+def evidence_to_array(evidenceDict, num, verbose=False):
+    array = np.empty((num ** 2, num ** 2))
+    for r1 in range(num):
+        for r2 in range(num):
+            for c1 in range(num):
+                for c2 in range(num):
+                    catVarKey = "pos_" + str(r1) + "_" + str(r2) + "_" + str(c1) + "_" + str(c2) + "_cconstraint"
+                    if catVarKey in evidenceDict:
+                        if verbose:
+                            print(
+                                "Position {} known to be {}".format(
+                                    str(r1) + "_" + str(r2) + "_" + str(c1) + "_" + str(c2),
+                                    evidenceDict[catVarKey] + 1))
+                        array[r1 * num + r2, c1 * num + c2] = evidenceDict[catVarKey] + 1
+                    else:
+                        array[r1 * num + r2, c1 * num + c2] = 0
+    return array
+
+
+num = 2
 constraints = get_sudoku_constraints(num)
+
 cores = {}
 for key in constraints:
     cores = {**cores,
@@ -65,23 +100,33 @@ for key in constraints:
 print("Created {} cores for sudoku with side number {}.".format(len(cores), num))
 
 preEvidence = {
-    # "a_0_0_0_0_0" : 1,
     "a_0_1_0_0_1": 1,
-    # "a_1_0_0_0_2" : 1,
-    # "a_1_1_0_0_3" : 1,
-    # "a_0_0_0_1_2" : 1
+    "a_0_0_0_1_0": 1,
+
+    "a_0_1_1_0_3": 1,
+
+    "a_1_0_0_0_2": 1,
+
+    "a_0_0_1_0_2": 1
 }
 
 propagator = cp.ConstraintPropagator(
     {**cores,
-     **crc.create_evidenceCoresDict(preEvidence)}
+     **crc.create_evidenceCoresDict(preEvidence)},
+    verbose=False
 )
 propagator.propagate_cores()
 
 evidenceDict, multipleAssignmentColors, redundantCores, remainingCores = propagator.find_evidence_and_redundant_cores()
+
+## Sanity Check: This follows already from evidence {"a_0_1_0_0_1": 1}
 
 ## The 1 in the first position is in place 2
 print(evidenceDict["square_1_0_0_cconstraint"] == num)
 
 ## The 1 is not in place 2
 print(not evidenceDict["a_0_0_0_0_1"])
+
+# print(evidenceDict["pos_0_0_0_0"])
+resArr = evidence_to_array(evidenceDict, num)
+print(resArr)
