3eca9ecd70810c622b2f41395974195dadb834ed
Alex Goeßmann <alex.goessmann@web.de>
2024-10-11 14:27:38 +0200

    Removed numpy usage from encoding by refering to tensor encoding beyond relational encoding

--- tnreason/encoding/data_to_cores.py
+++ tnreason/encoding/data_to_cores.py
@@ -1,26 +1,9 @@
 from tnreason import engine
-import numpy as np
 
 dataCoreSuffix = "_dataCore"
 defaultDataColor = "j"
 
-def samples_to_map(samples, variableList):
-    return lambda k: samples[variableList].iloc[k].values
 
-def categorical_to_relational_encoding(sampleDf, atomKeys=None, dataColor=defaultDataColor, dimensionsDict=None,
-                                       coreType=engine.defaultCoreType, partitionDict=None):
-    if atomKeys is None:
-        atomKeys = list(sampleDf.columns)
-    if dimensionsDict is None:
-        dimensionsDict = {atomKey : 2 for atomKey in atomKeys}
-    return engine.create_partitioned_relational_encoding(
-        inshape=[sampleDf.values.shape[0]], outshape=[dimensionsDict[atomKey] for atomKey in atomKeys],
-        incolors=[dataColor], outcolors=atomKeys,
-        function=samples_to_map(sampleDf, atomKeys),
-        coreType=coreType,
-        nameSuffix=dataCoreSuffix,
-        partitionDict=partitionDict
-    )
 
 def create_data_cores(sampleDf, atomKeys=None, dataColor=defaultDataColor, interpretation="atomic", dimensionsDict=None,
                       coreType=engine.defaultCoreType, partitionDict=None):
@@ -31,21 +14,41 @@ def create_data_cores(sampleDf, atomKeys=None, dataColor=defaultDataColor, inter
     if atomKeys is None:
         atomKeys = list(sampleDf.columns)
     if dimensionsDict is None:
-        dimensionsDict = {atomKey : 2 for atomKey in atomKeys}
-    if interpretation=="categorical":
+        dimensionsDict = {atomKey: 2 for atomKey in atomKeys}
+    if interpretation == "categorical":
         return categorical_to_relational_encoding(sampleDf, atomKeys, dataColor, dimensionsDict=dimensionsDict,
                                                   coreType=coreType, partitionDict=partitionDict)
-    elif interpretation == "atomic": # Then only coreType == "NumpyTensorCore" supported
-            return {atomKey + dataCoreSuffix: atomValues_from_sampleDf(sampleDf, atomKey, dataColor)
+    elif interpretation == "atomic":
+        return {atomKey + dataCoreSuffix: atomValues_from_sampleDf(sampleDf, atomKey, dataColor)
                 for atomKey in atomKeys if atomKey in list(sampleDf.columns)}
     else:
         raise ValueError("Interpretation {} not understood!".format(interpretation))
 
+def categorical_to_relational_encoding(sampleDf, atomKeys=None, dataColor=defaultDataColor, dimensionsDict=None,
+                                       coreType=engine.defaultCoreType, partitionDict=None):
+    """
+    Relational Encoding of samples, which are interpreted as certain states of categorical variables.
+    """
+    if atomKeys is None:
+        atomKeys = list(sampleDf.columns)
+    if dimensionsDict is None:
+        dimensionsDict = {atomKey: 2 for atomKey in atomKeys}
+    return engine.create_partitioned_relational_encoding(
+        inshape=[sampleDf.values.shape[0]], outshape=[dimensionsDict[atomKey] for atomKey in atomKeys],
+        incolors=[dataColor], outcolors=atomKeys,
+        function= lambda k: sampleDf[atomKeys].iloc[k].values,
+        coreType=coreType,
+        nameSuffix=dataCoreSuffix,
+        partitionDict=partitionDict
+    )
+
+
 def atomValues_from_sampleDf(sampleDf, atomKey, dataColor):
+    """
+    Tensor Encoding of samples, which are interpreted as atomic uncertainties.
+    """
     dataNum = sampleDf.values.shape[0]
     dfEntries = sampleDf[atomKey].values
-    values = np.zeros(shape=(dataNum, 2))
-    for i in range(dataNum):
-        values[i, 1] = dfEntries[i]
-        values[i, 0] = 1 - dfEntries[i]
-    return engine.get_core()(values, [dataColor, atomKey], name=atomKey + dataCoreSuffix)
+    tensorFunc = lambda j, a: (1 - a) * (1 - dfEntries[int(j)]) + a * dfEntries[int(j)]
+    return engine.create_tensor_encoding(inshape=[dataNum, 2], incolors=[dataColor, atomKey], function=tensorFunc,
+                                         name=atomKey + dataCoreSuffix)
